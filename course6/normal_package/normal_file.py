8c42ecb6cb2b8b4772fa832ee04c4ca0d9710c1f
sobolevn <mail@sobolevn.me>
2017-06-16 19:01:11 +0300

    Adds source code for lecture6

---
+++ course6/normal_package/normal_file.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'sobolevn'
+
+
+class MyClass:
+    pass
+
+
+def my_function():
+    print('My function is working!')
+
+
+GLOBAL_VAR = 12
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    my_function()
---
+++ course6/not_a_package/my_module.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'sobolevn'
+
+
+def print_info():
+    print('I am printed!')
